391d8df4a89e985ba015e43a9037f5bcaba33df9
amaan287 <amaanmirza287@gmail.com>
2025-03-11 13:02:06 +0530

    read and write in a file

---
+++ gowiki/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+type Page struct{
+	Title string
+	Body []byte
+}
+func(p *Page) save() error{
+	filename:=p.Title + ".txt"
+	return os.WriteFile(filename,p.Body,0600)
+}
+
+func loadPage(title string) *Page{
+	filename:= title + ".txt"
+	body, err := os.ReadFile(filename)
+if err!=nil{
+	log.Fatal("Error reading file: ",err)
+}
+	return &Page{Title:title,Body:body}
+}
+func main(){
+p1:=&Page{Title: "testPage",Body: []byte("This is a sample page.")}
+p1.save()
+p2:= loadPage("testPage")
+
+fmt.Println(string(p2.Body))
+
+}
