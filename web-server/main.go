6162d0253dad92fbb024a9c2c650e646cf2f48a4
amaan287 <amaanmirza287@gmail.com>
2025-03-31 16:16:50 +0530

    update

--- web-server/main.go
+++ web-server/main.go
@@ -1,19 +1,70 @@
 package main
-import ("fmt" 
-"net/http"
-	"log"
-)
-func helloHandler(w http.ResponseWriter,r *http.Request){
 
-}
-func main(){
-	fileserver:= http.FileServer(http.Dir("./static"))
-	http.Handle("/",fileserver)
-	http.HandleFunc("/form",formHandler)
-	http.HandleFunc("/Hello",helloHandler)
-	fmt.Printf("Starting server at port 8080 \n")
-	if err := http.ListenAndServe(":8080",nil), err != nil{
-		log.Fatal(err)
-	} 
-	
+import (
+	"fmt"
+	"syscall"
+)
+
+func main() {
+	// Create a socket
+	serverFd, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, 0)
+	if err != nil {
+		fmt.Println("Socket error:", err)
+		return
+	}
+	defer syscall.Close(serverFd)
+	syscall.SetsockoptInt(serverFd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1)
+
+	// Bind socket to address and port
+	addr := syscall.SockaddrInet4{Port: 8080}
+	copy(addr.Addr[:], []byte{127, 0, 0, 1}) // localhost
+
+	err = syscall.Bind(serverFd, &addr)
+	if err != nil {
+		fmt.Println("Bind error:", err)
+		return
+	}
+
+	// Start listening
+	err = syscall.Listen(serverFd, 10)
+	if err != nil {
+		fmt.Println("Listen error:", err)
+		return
+	}
+
+	fmt.Println("Server is running on http://127.0.0.1:8080")
+
+	for {
+		// Accept a new connection
+		clientFd, _, err := syscall.Accept(serverFd)
+		if err != nil {
+			fmt.Println("Accept error:", err)
+			continue
+		}
+
+		// Read request
+		buf := make([]byte, 1024)
+		_, err = syscall.Read(clientFd, buf)
+		if err != nil {
+			fmt.Println("Read error:", err)
+			syscall.Close(clientFd)
+			continue
+		}
+
+		// HTTP Response
+		response := "HTTP/1.1 200 OK\r\n" +
+			"Content-Type: text/plain\r\n" +
+			"Content-Length: 13\r\n" +
+			"\r\n" +
+			"Hello, World!"
+
+		// Send response
+		_, err = syscall.Write(clientFd, []byte(response))
+		if err != nil {
+			fmt.Println("Write error:", err)
+		}
+
+		// Close client connection
+		syscall.Close(clientFd)
+	}
 }
